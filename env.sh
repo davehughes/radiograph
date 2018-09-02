a8bba57f6d25ceb2181b91410a4a7c7aa7390a72
David Hughes <d@vidhughes.com>
2018-09-02 14:20:11 -0700

    Take more variables from the environment

--- env.sh
+++ env.sh
@@ -2,3 +2,9 @@
 export SECRET_KEY=
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
+
+export DB_NAME=radiograph
+export DB_USER=radiograph
+export DB_PASSWORD=radiograph
+export DB_HOST=localhost
+export DB_PORT=5432
--- radiograph/settings.py
+++ radiograph/settings.py
@@ -19,11 +19,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'radiograph',
-        'USER': 'radiograph',
-        'PASSWORD': 'radiograph',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
